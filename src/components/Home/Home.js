02aa5a76fd2df2979668d9b1bf0baa0f07e186ae
Faisal Ahammad <faisalahammad24@gmail.com>
2022-01-29 22:28:21 +0600

    RMP - Home page content added and some spacing issue has been fixed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import FaisalImg from '../../images/faisal.jpg';
 
 const Home = () => {
   return (
     <div className="home">
-      <button type="submit">Home Page</button>
+      <img src={FaisalImg} alt="Faisal Ahammad" className='profile-pic' />
+      <p>Hi,<br />
+        I am <a href="https://github.com/faisalahammad" target="_blank" rel='noreferrer'>Faisal Ahammad</a>. I'm learning React and wanna be a good MERN Developer. I know those keywords-
+      </p>
+      <ul>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>PHP</li>
+        <li>WordPress</li>
+        <li>Shopify</li>
+        <li><mark>CrateJoy</mark></li>
+      </ul>
+      <p>Oh, I forgot to tell you, you've go to <a href="/blog">Blog</a> page to check all blog post.</p>
     </div>
   );
 };
